fac3257631d0f08dae9ee96c8b31e222953a225a
jlhdez <jorgelhdez12@gmail.com>
2017-07-15 21:29:31 -0400

    Playing with first tutorial

--- windows/projects/BasicImageContainer/BasicImageContainer/Main.cpp
+++ windows/projects/BasicImageContainer/BasicImageContainer/Main.cpp
@@ -6,42 +6,38 @@ using namespace cv;
 
 int main(int argc, char ** argv)
 {
-	std::cout << "This tutorial goes over the class Mat" << std::endl;
+	std::cout << "This tutorial goes over a small portion of the class Mat" << std::endl;
 
-	/*
-	Mat A, C;									// Mat declaration, creates just the header parts
-	A = imread(argv[1], IMREAD_COLOR);			// Loads an image from a file
-												//	* If image cannot be loaded imread returns an empty Matrix (i.e. Mat::data == NULL) 
-												//	* Has multiple flags for different options, e.g. IMREAD_COLOR
+	char* imageName = argv[1]; 
 
-	Mat B(A); C = A;							// Mat A, B, and C all have different headers, but share the same data
-												
-												// Mat D and E share the same data but have a "region of interest"
-	Mat D (A, Rect(10, 10, 100, 100) );			// ROI using a rectangle
-	Mat E = A(Range::all(), Range(1,3));		// ROI using row and column boundaries
-
-	Mat F = A.clone();							// Mat F and G now have a copy of the data of A. Modifying Mat F and G will not affect Mat A
-	Mat G;  A.copyTo(G);
-	
-	*/
-	char* imageName = argv[1];
 	Mat image;
-	image = imread(imageName, 1);
-	
-	if (image.data == nullptr)
+	// Store image in Mat `image`
+	image = imread(imageName, 1); 
+
+	// Check if everything went well
+	if (image.data == nullptr) 
 	{
 		std::cout << "Couldn't open Image" << std::endl;
 		return 0;
 	}
 
 	Mat gray_image;
-	cvtColor(image, gray_image, COLOR_BGR2GRAY);
-	imwrite("Gray_Image.jpg", gray_image);
-	//namedWindow(imageName, WINDOW_AUTOSIZE);
-	//namedWindow("Gray image", WINDOW_AUTOSIZE);
-	//imshow(imageName, image);
-	//imshow("Gray image", gray_image);
-	//waitKey(0);
 
+	// Convert image to greyscale
+	cvtColor(image, gray_image, COLOR_BGR2GRAY); 
+
+	// Write the image
+	imwrite("Gray_Image.jpg", gray_image); 
+
+	// Get ready to display images
+	namedWindow(imageName, WINDOW_AUTOSIZE);
+	namedWindow("Gray image", WINDOW_AUTOSIZE);
+
+	// Show the images
+	imshow(imageName, image);
+	imshow("Gray image", gray_image);
+	// Wait for user key press
+	waitKey(0);
+	
 	return 0;
 }
---
+++ windows/projects/BasicImageContainer/NOTES.md
@@ -0,0 +1,22 @@
+# Notes
+
+1. Mat declaration, creates just the header parts
+1. Loads an image from a file
+ + If image cannot be loaded, `imread` returns an empty Matrix (i.e. Mat::data == NULL)
+ + Has multiple flags for different options, e.g. IMREAD_COLOR
+1. Mat A, B, and C all have different headers, but share the same data
+1. Mat D and E share the same data but have a _region of interest_ (ROI)
+1. Mat F and G now have a copy of the data of A. Modifying Mat F and G will not affect Mat A
+
+```
+Mat A, C;									              // 1
+A = imread(argv[1], IMREAD_COLOR);			// 2
+
+Mat B(A); C = A;							         // 3
+                                       // 4
+Mat D (A, Rect(10, 10, 100, 100) );		 // ROI using a rectangle
+Mat E = A(Range::all(), Range(1,3));	 // ROI using row and column boundaries
+
+Mat F = A.clone();							       // 5
+Mat G;  A.copyTo(G);
+```
